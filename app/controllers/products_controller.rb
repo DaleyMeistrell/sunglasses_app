97821d2e8d8bc055abaf2cdb5e1d2bfd6e1e03b1
Daley Meistrell <dstrell14@gmail.com>
2015-06-05 17:15:54 -0700

    push to heroku 4

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
 	def update
 		@product = Product.find(params[:id])
 		if @product.update_attributes(params.require(:product).permit(:name, :price, :weight, :inventory, :description, :image))
-			redirect_to products_path
+			redirect_to product_path(@product.name)
 		else
 			render :edit
 		end
